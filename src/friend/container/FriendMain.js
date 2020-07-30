80126a361118f9006adc64599e52df0ce064ed49
Maverick.b <maverick.b@kakaocorp.com>
2020-07-31 07:49:09 +0900

    react-redux로 사용해보기

--- src/friend/container/FriendMain.js
+++ src/friend/container/FriendMain.js
@@ -4,28 +4,34 @@ import { getNextFriend } from "../../common/mockData";
 import { addFriend } from "../state";
 import FriendList from "../component/FriendList";
 
+import { useSelector, useDispatch } from "react-redux";
+
 export default function FriendMain() {
-  const [, forceUpdate] = useReducer(v => v + 1, 0);
-  useEffect(() => {
-    let prevFriends = store.getState().friend.friends;
-    const unsubscribe = store.subscribe(() => {
-      const friends = store.getState().friend.friends;
-      if (prevFriends !== friends) {
-        forceUpdate();
-      }
-      prevFriends = friends;
-    });
-    return () => unsubscribe();
-  }, []);
+  const friends = useSelector(state => state.friend.friends);
+  const dispatch = useDispatch();
+
+
+  // const [, forceUpdate] = useReducer(v => v + 1, 0);
+  // useEffect(() => {
+  //   let prevFriends = store.getState().friend.friends;
+  //   const unsubscribe = store.subscribe(() => {
+  //     const friends = store.getState().friend.friends;
+  //     if (prevFriends !== friends) {
+  //       forceUpdate();
+  //     }
+  //     prevFriends = friends;
+  //   });
+  //   return () => unsubscribe();
+  // }, []);
 
   function onAdd() {
     const friend = getNextFriend();
-    store.dispatch(addFriend(friend));
+    dispatch(addFriend(friend));
   }
 
   console.log("FriendMain render");
 
-  const friends = store.getState().friend.friends;
+  // const friends = store.getState().friend.friends;
 
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDom from "react-dom";
 import TimelineMain from "./timeline/container/TimelineMain";
 import FriendMain from "./friend/container/FriendMain";
 
+import store from "./common/store";
+import { Provider } from "react-redux";
+
 ReactDom.render(
-  <div>
-    <FriendMain />
-    <TimelineMain />
-  </div>,
+  <Provider store={store}>
+    <div>
+      <FriendMain />
+      <TimelineMain />
+    </div>
+  </Provider>,
   document.getElementById("root")
 );
